e91590ee479b4f80703708ddc38e5312a4ec903c
deyang_xing <deyang_xing@kingdee.com>
2023-02-21 15:47:08 +0800

    接口整体验证ok

--- config/index.js
+++ config/index.js
@@ -1,5 +1,7 @@
 module.exports = {
-    defaultHost: 'kdtest.kdweibo.cn',
-    // defaultHost: 'www.yunzhijia.com',
-    cookie: 'toweibologin=login; _jzqa=1.2417462718415430700.1671068986.1671068986.1671068986.1; _jzqx=1.1671068986.1671068986.1.jzqsr=yunzhijia.com|jzqct=/yzj-layout/home/.-; _qzja=1.254113557.1671068986457.1671068986457.1671068986457.1671068986457.1671068986457..0.0.1.1; parent_qimo_sid_ce3d5ef0-6836-11e6-85a2-2d5b0666fd02=bc648eab-8adb-4c99-958d-cc4df19b68db; accessId=ce3d5ef0-6836-11e6-85a2-2d5b0666fd02; gl=e5655d55-76fe-4daf-8771-f0a386dd8463; gl=e5655d55-76fe-4daf-8771-f0a386dd8463; Hm_lvt_45f5f201f5af9cfeffd1f82177d2cceb=1675826117,1675931088; Hm_lpvt_45f5f201f5af9cfeffd1f82177d2cceb=1675931088; Hm_lvt_a96914087b350d1aa86c96cdbf56d5e5=1675066750,1675931088; href=https://www.yunzhijia.com/home/; Hm_lpvt_a96914087b350d1aa86c96cdbf56d5e5=1675931090; pageViewNum=7; cd=yunzhijia.com; cn=5b5420f2e4b02abb5464c7dd; cu=4fb535b524ac268f12bf00ed; at=e16d9afe-86cb-4704-8057-f3cc84fe02a9; uuid=4e8c130d-391c-475b-8367-6915bc20f93a; webLappToken="RTXcirUwxXdcfp55wQk7+f4+Z5/GZzus0EVfOy9jniavt6vvFJNTvAvatsPHvWIVHPVdz1tgKAWGh5q5CYi2xTo7CiP3iaT6cUpndaXu3U8="; sync_networkid=5b5420f2e4b02abb5464c7dd; sync_userid=4fb535b524ac268f12bf00ed; cd=yunzhijia.com; cn=5b5420f2e4b02abb5464c7dd'
+    //defaultHost: 'kdtest.kdweibo.cn',
+    defaultHost: 'www.yunzhijia.com',
+    txCloudHost: '119.29.59.155:3003',
+    token: 'QRMWSQA7teyVkHHbZQQbTVgwF8UOHaD6',
+    cookie: 'JSESSIONID=node0gd7jbgvv4o58f2zcmx4mm3sq36.node0; _jzqa=1.2417462718415430700.1671068986.1671068986.1671068986.1; _jzqx=1.1671068986.1671068986.1.jzqsr=yunzhijia%2Ecom|jzqct=/yzj-layout/home/.-; Hm_lvt_a96914087b350d1aa86c96cdbf56d5e5=1675066750,1675931088; cd=yunzhijia.com; logintype4fb535b524ac268f12bf00ed=0; __loginType=; redirectIndexUrl=/im/xiaoxi; __att_token=ZhyhQvuIu9uDjjoiSYKcjsrtY4UPbWb6; lappKey=lightapp_05dc3e49135d0b3d30d9b7e7102dabfd; cn=5b5420f2e4b02abb5464c7dd; Hm_lpvt_a96914087b350d1aa86c96cdbf56d5e5=1676625129; uuid=b651e07f-df4e-4ae5-82dd-5c44f64265e7; at=d87b4546-2e3b-4c73-ba78-e9dc13575b1f; __signtoken=QRMWSQA7teyVkHHbZQQbTVgwF8UOHaD6'
 }
--- index.js
+++ index.js
@@ -6,8 +6,8 @@
     4. 审批付费（1：付费 0：非付费）
     5. 权限业务中心（1：开启 0：未开启）
     6. 启用新考勤（1：启用 0：未启用）
-    7. 调休假方案（1：等于一 0：大于一）
-    8. 考勤机同步（1：使用 2：未使用）
+    7. 调休假方案大于1（1：大于一 0：小于等于一）
+    8. 考勤机使用（1：使用 2：未使用）
     9. 迁移状态（1：已迁移 2：不迁移 0：未迁移）
     10. networkId
     11. 工作圈名称
@@ -18,6 +18,11 @@ const readline          = require('readline')
 const getQdFfInfo       = require('./lib/check-qdff')
 const getQdHuoYue       = require('./lib/check-qdhy')
 const getSpFfInfo       = require('./lib/check-spff')
+const getQxywInfo       = require('./lib/check-qxyw')
+const getUseNew         = require('./lib/check-useNew')
+const getUseMachine     = require('./lib/check-useMachine')
+const getTransStatus    = require('./lib/check-transStatus')
+const getNetworkInfo    = require('./lib/check-networkInfo')
 
 async function init () {
     const rl = readline.createInterface({
@@ -27,12 +32,19 @@ async function init () {
     for await (const eid of rl) {
         console.log('当前处理eid: %s', eid)
         try {
+            const c1 = eid
             const c2 = await getQdFfInfo(eid)
             const c3 = await getQdHuoYue(eid)
             const c4 = await getSpFfInfo(eid)
-            console.log(c3)
+            const c5 = await getQxywInfo(eid)
+            let {hasUse, hasMultiRules} = await getUseNew(eid)
+            const c6 = hasUse ? 1 : 0
+            const c7 = hasMultiRules ? 1: 0
+            const c8 = await getUseMachine(eid)
+            const c9 = await getTransStatus(eid)
+            let {networkId: c10, name: c11} = await getNetworkInfo(eid)
         } catch (err) {
-            console.log('%s, 提取信息出现异常: %s', eid, err)
+            console.log('%s, 提取信息过程出现异常: %s', eid, err)
         }
     }
 }
