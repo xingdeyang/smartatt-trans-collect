e6e5019d77f12cdc02acc9b6ce8f60010b0b1501
deyang_xing <deyang_xing@kingdee.com>
2023-02-22 11:33:00 +0800

    脚本整体自测完成

--- config/index.js
+++ config/index.js
@@ -2,6 +2,6 @@ module.exports = {
     //defaultHost: 'kdtest.kdweibo.cn',
     defaultHost: 'www.yunzhijia.com',
     txCloudHost: '119.29.59.155:3003',
-    token: 'QRMWSQA7teyVkHHbZQQbTVgwF8UOHaD6',
-    cookie: 'JSESSIONID=node0gd7jbgvv4o58f2zcmx4mm3sq36.node0; _jzqa=1.2417462718415430700.1671068986.1671068986.1671068986.1; _jzqx=1.1671068986.1671068986.1.jzqsr=yunzhijia%2Ecom|jzqct=/yzj-layout/home/.-; Hm_lvt_a96914087b350d1aa86c96cdbf56d5e5=1675066750,1675931088; cd=yunzhijia.com; logintype4fb535b524ac268f12bf00ed=0; __loginType=; redirectIndexUrl=/im/xiaoxi; __att_token=ZhyhQvuIu9uDjjoiSYKcjsrtY4UPbWb6; lappKey=lightapp_05dc3e49135d0b3d30d9b7e7102dabfd; cn=5b5420f2e4b02abb5464c7dd; Hm_lpvt_a96914087b350d1aa86c96cdbf56d5e5=1676625129; uuid=b651e07f-df4e-4ae5-82dd-5c44f64265e7; at=d87b4546-2e3b-4c73-ba78-e9dc13575b1f; __signtoken=QRMWSQA7teyVkHHbZQQbTVgwF8UOHaD6'
+    token: 'i98RCOzPdfdTj0kYHWy9e2pvWpEx7v6B',
+    cookie: 'JSESSIONID=node01d064vywbih2c1d5606uhyxrcy0.node0; _jzqa=1.2417462718415430700.1671068986.1671068986.1671068986.1; _jzqx=1.1671068986.1671068986.1.jzqsr=yunzhijia%2Ecom|jzqct=/yzj-layout/home/.-; Hm_lvt_a96914087b350d1aa86c96cdbf56d5e5=1675066750,1675931088; cd=yunzhijia.com; logintype4fb535b524ac268f12bf00ed=0; __loginType=; redirectIndexUrl=/im/xiaoxi; __att_token=ZhyhQvuIu9uDjjoiSYKcjsrtY4UPbWb6; lappKey=lightapp_05dc3e49135d0b3d30d9b7e7102dabfd; Hm_lpvt_a96914087b350d1aa86c96cdbf56d5e5=1676625129; uuid=b651e07f-df4e-4ae5-82dd-5c44f64265e7; at=d87b4546-2e3b-4c73-ba78-e9dc13575b1f; gl=36c58ef0-7fe0-49e3-9d44-eb1518164f79; cn=5b5420f2e4b02abb5464c7dd; __signtoken=i98RCOzPdfdTj0kYHWy9e2pvWpEx7v6B'
 }
--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 /*
-    汇总结果字段顺序
-    1. eid
-    2. 考勤付费（1：付费 0：非付费）
-    3. 签到活跃（1：活跃 0：非活跃）
-    4. 审批付费（1：付费 0：非付费）
-    5. 权限业务中心（1：开启 0：未开启）
-    6. 启用新考勤（1：启用 0：未启用）
-    7. 调休假方案大于1（1：大于一 0：小于等于一）
-    8. 考勤机使用（1：使用 2：未使用）
-    9. 迁移状态（1：已迁移 2：不迁移 0：未迁移）
-    10. networkId
-    11. 工作圈名称
+    一、nodejs版本：14.17.0
+    二、汇总结果字段顺序
+        1. eid
+        2. 签到付费（1：付费 0：非付费）
+        3. 签到活跃（1：活跃 0：非活跃）
+        4. 审批付费（1：付费 0：非付费）
+        5. 权限业务中心开启（1：开启 0：未开启）
+        6. 使用过新考勤（1：启用 0：未启用）
+        7. 有多条调休假计算方案（1: 是 0：否）
+        8. 使用考勤机（1：使用 2：未使用）
+        9. 迁移状态（1：已迁移 2：不迁移 0：未迁移）
+        10. networkId
+        11. 工作圈名称
 */
-const fs                = require('fs')
-const path              = require('path')
-const readline          = require('readline')
-const getQdFfInfo       = require('./lib/check-qdff')
-const getQdHuoYue       = require('./lib/check-qdhy')
-const getSpFfInfo       = require('./lib/check-spff')
-const getQxywInfo       = require('./lib/check-qxyw')
-const getUseNew         = require('./lib/check-useNew')
-const getUseMachine     = require('./lib/check-useMachine')
-const getTransStatus    = require('./lib/check-transStatus')
-const getNetworkInfo    = require('./lib/check-networkInfo')
+const fs                        = require('fs')
+const { rm, open, writeFile }   = require('fs').promises
+const path                      = require('path')
+const readline                  = require('readline')
+const getQdFfInfo               = require('./lib/check-qdff')
+const getQdHuoYue               = require('./lib/check-qdhy')
+const getSpFfInfo               = require('./lib/check-spff')
+const getQxywInfo               = require('./lib/check-qxyw')
+const getUseNew                 = require('./lib/check-useNew')
+const getUseMachine             = require('./lib/check-useMachine')
+const getTransStatus            = require('./lib/check-transStatus')
+const getNetworkInfo            = require('./lib/check-networkInfo')
+const resultTitle               = `eid\t签到付费\t签到活跃\t审批付费\t权限业务中心开启\t使用过新考勤\t有多条调休假计算方案\t使用考勤机\t迁移状态\tnetworkId\t工作圈名称\n`
 
 async function init () {
+    const resultFilePath = path.resolve(__dirname, './result.txt')
+    await rm(resultFilePath)
+    const fd = await open(resultFilePath, 'a')
+    await writeFile(fd, resultTitle)
     const rl = readline.createInterface({
         input: fs.createReadStream(path.resolve(__dirname, './eid.txt')),
         outpu: process.stdout
@@ -37,12 +44,14 @@ async function init () {
             const c3 = await getQdHuoYue(eid)
             const c4 = await getSpFfInfo(eid)
             const c5 = await getQxywInfo(eid)
-            let {hasUse, hasMultiRules} = await getUseNew(eid)
+            let { hasUse, hasMultiRules } = await getUseNew(eid)
             const c6 = hasUse ? 1 : 0
             const c7 = hasMultiRules ? 1: 0
             const c8 = await getUseMachine(eid)
             const c9 = await getTransStatus(eid)
-            let {networkId: c10, name: c11} = await getNetworkInfo(eid)
+            let { networkId: c10, name: c11 } = await getNetworkInfo(eid)
+            const lineData = `${c1}\t${c2}\t${c3}\t${c4}\t${c5}\t${c6}\t${c7}\t${c8}\t${c9}\t${c10}\t${c11}\n`
+            await writeFile(fd, lineData)
         } catch (err) {
             console.log('%s, 提取信息过程出现异常: %s', eid, err)
         }
